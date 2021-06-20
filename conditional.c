258f3d5ef843c305635398a3d296ddf2ebbe1715
shasank253 <85795995+shasank253@users.noreply.github.com>
2021-06-20 20:50:26 +0530

    created conditional.c
    it shows conditional statement

---
+++ conditional.c
@@ -0,0 +1,27 @@
+#include<stdio.>
+int main()
+{
+  int choice=0;
+  printf("\n choose item \n1:pen \n2:book \n3:pencil \n4:eraser \n5:sharpner");
+  scanf("%d",&choice);
+  switch(choce)
+  {   case 1:
+  printf("pen");
+  break;
+  case 2:
+  printf("book");
+  break;
+  case 3:
+  printf("pencil");
+  break;
+  case 4:
+  printf("eraser");
+  break;
+  case 5:
+  printf("sharpner");
+  break;
+  default:
+  printf("invallid");
+  }
+  return 0;
+}
